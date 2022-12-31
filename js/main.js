eeac0cf99a60c52d187a383c3f70360a40d2369b
KimberlyDaniJ <kimberlydanij@gmail.com>
2022-12-31 23:08:48 +0900

    20221231 | Add

---
+++ js/main.js
@@ -0,0 +1,44 @@
+////////////////////////////////////
+// 슬라이딩 구현
+
+setInterval(function() {
+let tg = document.querySelector('.slide');
+tg.style.top = '-100%';
+tg.style.transition = '0.6s';
+
+setTimeout(function()  {
+    let first = tg.querySelectorAll('li')[0];
+    //대상의 top값이 -100%으로 0.6초 동안 transition이 발생
+    console.log(`first:${first}`)
+    tg.appendChild(first);
+    //대상의 top값이 0으로 이동한다.
+    tg.style.top = '0';
+    tg.style.transition = none;
+    }, 600); //0.6초
+}, 3000); //3초
+
+/////////////////////////////////
+// 공지사항 및 갤러리 구현
+
+function chgTab(i1, i2) {
+    //i1: class를 넣은 li순번
+    //i2: class를 뺄 li 순번
+
+    let tm = document.querySelectorAll(".tm>li");
+    let tc = document.querySelectorAll(".tc>li");
+
+    //on클래스 넣기
+    tm[i1].classList.add('on');
+    tc[i1].classList.add('on');
+
+       //on클래스 빼기
+    tm[i2].classList.remove('on');
+    tc[i2].classList.remove('on');
+}
+
+////////////////////
+// 팝업창 닫기
+function popup(sts) {
+    let pop = document.querySelector('.pop');
+    pop.style.display= sts;
+}
