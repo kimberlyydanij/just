b6d3dba883b81c7e9fd920661d1697ff67ebc94f
kimberlyydanij <kimberlydanij@gmail.com>
2023-03-25 22:45:15 +0900

    20230325 | Update

---
+++ MessageBeanEnglish.java
@@ -0,0 +1,13 @@
+package sample02;
+
+public class MessageBeanEnglish  implements MessageBean{
+		public MessageBeanEnglish() {
+	
+		} 
+		
+		public void sayHello(String name) {
+			System.out.printf("Hello, %s !!!",name);
+			
+		} // end sayHello
+
+} // end class
---
+++ MessageBeanKorea.java
@@ -0,0 +1,9 @@
+package sample02;
+
+public class MessageBeanKorea {
+	
+	public void sayHello(String name) {
+		System.out.printf("안녕, %s !!!",name);
+		
+	} // end sayHello
+}
