73ea44a2562b7abadb20c70fb206ce6a35433c47
kimberlyydanij <kimberlydanij@gmail.com>
2023-05-02 23:18:48 +0900

    20230502 | Update

--- App.js
+++ App.js
@@ -1,55 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
-import MyJsx001 from './components/ex01_jsx/MyJsx001';
-import MyJsx002 from './components/ex01_jsx/MyJsx002';
-import MyJsx003 from './components/ex01_jsx/MyJsx003';
-import MyJsx004 from './components/ex01_jsx/MyJsx004';
-import MyJsx005 from './components/ex01_jsx/MyJsx005';
-import MyBasic001 from './components/ex02_state/MyBasic001';
-import MyuseState002 from './components/ex02_state/MyuseState002';
-import MyuseState003 from './components/ex02_state/MyuseState003';
-import MyuseState004 from './components/ex02_state/MyuseState004';
-import MyuseState005 from './components/ex02_state/MyuseState005';
-import MyuseState006 from './components/ex02_state/MyuseState006';
-import MyuseRef001 from './components/ex03_useRef/MyuseRef001';
-import MyuseRef002 from './components/ex03_useRef/MyuseRef002';
-import MyuseEffect001 from './components/ex04_useEffect/MyuseEffect001';
-import MyuseEffect002 from './components/ex04_useEffect/MyuseEffect002';
-import MyuseStatesync001 from './components/ex05_async/MyuseStatesync001';
-import MyuseStatesync002 from './components/ex05_async/MyuseStatesync002';
-import MyuseReducer001 from './components/ex06_useReducer/MyuseReducer001';
-import MyuseReducer002 from './components/ex06_useReducer/MyuseReducer002';
-import Myprops001 from './components/ex07_props/Myprops001';
-import Myprops002 from './components/ex07_props/Myprops002';
-import Myprops003 from './components/ex07_props/Myprops003';
-import MyuseContext01 from './components/ex08_context/MyuseContext01';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Home from "./components/Home";
+import BaseLayout from "./components/layout/BaseLayout";
+import BoardList from "./components/board/board_list";
+import BoardView from "./components/board/board_view";
+import BoardWrite from "./components/board/board_write";
+import BoardUpdate from "./components/board/board_update";
+import JoinForm from "./components/login/JoinForm";
+import LoginPage from "./components/login/LoginPage";
+import LogOut from "./components/login/LogOut";
 
 function App() {
   return (
-    <div className='App'>
-      {/* {<MyJsx001 />} */}
-      {/* {<MyJsx002 />} */}
-      {/* {<MyJsx003 />} */}
-      {/* {<MyJsx004 />} */}
-      {/* {<MyJsx005 />} */}
-      {<MyBasic001 />}
-      {/* {<MyuseState002 />} */}
-      {/* {<MyuseState003 />} */}
-      {/* {<MyuseState004 />} */}
-      {/* {<MyuseState005 />} */}
-      {/* {<MyuseState006 />} */}
-      {/* {<MyuseRef001 />} */}
-      {/* {<MyuseRef002 />} */}
-      {/* {<MyuseEffect001 />} */}
-      {/* {<MyuseEffect002 />} */}
-      {/* {<MyuseStatesync001 />} */}
-      {/* {<MyuseStatesync002 />} */}
-      {/* {<MyuseReducer001 />} */}
-      {/* {<MyuseReducer002 />} */}
-      {/* {<Myprops001 name="홍길동" />} */}
-      {/* {<Myprops002 name="홍길동" age="27" loc="강남구" />} */}
-      {/* {<Myprops003 name="홍길동" loc="제주시" />} */}
-      {/*<MyuseContext01 /> */}
+    <div className="container">
+      <h1>My Shop Board</h1>
+      <Routes>
+        <Route path="/" element={<BaseLayout />}>
+          <Route index element={<Home />} />
+
+          <Route path="login" element={<LoginPage />} />
+          <Route path="logout" element={<LogOut />} />
+
+          <Route path="join" element={<JoinForm />} />
+
+          <Route path="board" element={<BoardList />}>
+            <Route path="list/:currentPage" element={<BoardList />} />
+          </Route>
+
+          <Route path="board/view/:currentPage/:num" element={<BoardView />} />
+
+          <Route path="board/write" element={<BoardWrite />} />
+          <Route
+            path="board/write/:currentPage/:num/:ref/:re_step/:re_level"
+            element={<BoardWrite />}
+          />
+
+          <Route
+            path="board/update/:currentPage/:num"
+            element={<BoardUpdate />}
+          />
+        </Route>
+      </Routes>
     </div>
   );
 }
