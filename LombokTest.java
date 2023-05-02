481b91720ac7a11de389899dcc19872aa67343e7
kimberlyydanij <kimberlydanij@gmail.com>
2023-05-03 00:20:21 +0900

    20230503 | Update

--- LombokTest.java
+++ LombokTest.java
@@ -1,49 +1,16 @@
-package part01;
+package com.example.demo;
 
 public class LombokTest {
 
 	public static void main(String[] args) {
-		MemberVO mv = new MemberVO();
-		mv.setName("홍길동");
-		mv.setAge(30);
-		mv.setLoc("서울");
-
-		System.out.printf("%s %d %s\n", mv.getName(), mv.getAge(), mv.getLoc());
-		// part01.MemberVO@28f67ac7
-		// MemberVO(name=홍길동, age=30, loc=서울)
-		System.out.println(mv.toString());
-
-		mv = new MemberVO("여진구", 28, "경기");
-
-		System.out.printf("%s %d %s\n", mv.getName(), mv.getAge(), mv.getLoc());
-		// part01.MemberVO@256216b3
-		// MemberVO(name=여진구, age=28, loc=경기)
-		System.out.println(mv.toString());
-
-		MemberVO vo1 = new MemberVO("여진구", 28, "경기");
-		MemberVO vo2 = new MemberVO("여진구", 28, "경기");
-		MemberVO vo3 = new MemberVO("여진구", 28, "제주");
-		// false
-		// true
-		System.out.println(vo1.equals(vo2));
-		// false
-		System.out.println(vo1.equals(vo3));
-		// 1918627686
-		// -1323333202
-		System.out.println(vo1.hashCode());
-		// 716143810
-		// -1323333202
-		System.out.println(vo2.hashCode());
-		// -1323109125
-		System.out.println(vo3.hashCode());
-
-		//mv = new MemberVO(null, 0, null);
-		//System.out.printf("%s %d %s\n", mv.getName(), mv.getAge(), mv.getLoc());
+		MemDTO dto = new MemDTO();
+		dto.setName("홍길동");
+		dto.setAge(30);
+		dto.setLoc("서울");
 		
-		mv = new MemberVO("차현우","광주");
-		System.out.printf("%s %d %s\n", mv.getName(), mv.getAge(), mv.getLoc());
+		System.out.printf("%s %d %s\n", dto.getName(), dto.getAge(), dto.getLoc());
 		
-		
+		MemDTO dto2 = new MemDTO("전혜빈", 25, "대전");
+		System.out.printf("%s %d %s\n", dto2.getName(), dto2.getAge(), dto2.getLoc());
 	}
-
 }
