a0c677ead6cc019aeed8f15bdb34bd52bc047683
kimberlyydanij <kimberlydanij@gmail.com>
2023-06-04 23:19:03 +0900

    20230604 | UpdatE

--- app1.js
+++ app1.js
@@ -13,6 +13,21 @@ const calculator = {
   },
 };
 
+const calculator1 = {
+  add: function (a, b) {
+    console.log(a, b);
+  },
+  minus: function (a, b) {
+    console.log(a - b);
+  },
+  multiply: function (a, b) {
+    console.log(a * b);
+  },
+  divide: function (a, b) {
+    console.log(a / b);
+  },
+};
+
 calculator.add(5, 2);
 calculator.minus(7, 2);
 calculator.multiply(3, 4);
