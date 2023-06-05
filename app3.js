f0e4f88e5508d54775f13f5091e6fa42e57f9c36
kimberlyydanij <kimberlydanij@gmail.com>
2023-06-05 23:31:43 +0900

    20230605 | Update

--- app3.js
+++ app3.js
@@ -3,3 +3,11 @@ const age = parseInt(prompt('how old are you?'));
 if (isNaN(age)) {
   console.log('please write a number');
 } else console.log('thank you for writing your age');
+
+if (isNaN(age)) {
+  console.log('please write a number');
+} else console.log('thank you for writing your age');
+
+if (isNaN(age)) {
+  console.log('please write a number');
+} else console.log('thank you for writing your age');
