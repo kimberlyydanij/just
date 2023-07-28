7afa443af468583b21d05aaa95058aa2d7c0afda
kimberlyydanij <kimberlydanij@gmail.com>
2023-07-28 23:56:57 +0900

    20230728 | Update

---
+++ T1000.java
@@ -0,0 +1,14 @@
+package coding_test;
+
+import java.util.Scanner;
+
+public class T1000 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		double a = sc.nextInt();
+		double b = sc.nextInt();
+		double answer = a/b;
+		System.out.println(answer);
+	}
+	}
