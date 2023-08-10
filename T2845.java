63bbeb3f0f22cf601d936a5ef942df44128880e7
kimberlyydanij <kimberlydanij@gmail.com>
2023-08-10 23:23:16 +0900

    20230810 | Update

---
+++ T2845.java
@@ -0,0 +1,20 @@
+package coding_test;
+import java.util.Scanner;
+
+public class T2845 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		int d = sc.nextInt();
+		int e = sc.nextInt();
+		int f = sc.nextInt();
+		int g = sc.nextInt();
+		int sum = a * b;
+		sc.close();
+		System.out.printf("%d %d %d %d %d",c-sum,d-sum,e-sum,f-sum,g-sum);
+	}
+}
