5339dfca5a83d02450aa38cb9de09eea58df1bd9
kimberlyydanij <kimberlydanij@gmail.com>
2023-08-19 23:54:36 +0900

    20230819 | Update

---
+++ T10699.java
@@ -0,0 +1,11 @@
+package coding_test;
+
+import java.time.LocalDate;
+
+public class T10699 {
+
+	public static void main(String[] args) {
+		LocalDate today = LocalDate.now();
+		System.out.println(today);
+	}
+}
