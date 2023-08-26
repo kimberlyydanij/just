8cd345e757068cb5a8673040d9e755c480498444
kimberlyydanij <kimberlydanij@gmail.com>
2023-08-26 23:48:14 +0900

    20230826 | Update

---
+++ T6840.java
@@ -0,0 +1,17 @@
+package coding_test;
+
+import java.util.Scanner;
+
+public class T6840 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a[] = new int[3];
+		int min = 0;
+		for(int i = 0;i < 3;i++) {
+			a[i] = sc.nextInt();
+		}
+
+	}
+
+}
