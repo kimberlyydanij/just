f4a8bd185088223caec77b19b6c714d33ba9b427
kimberlyydanij <kimberlydanij@gmail.com>
2023-08-29 01:58:26 +0900

    20230829 | Update

---
+++ T8370.java
@@ -0,0 +1,16 @@
+package coding_test;
+
+import java.util.Scanner;
+
+public class T8370 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		int d = sc.nextInt();
+		sc.close();
+		System.out.println((a*b)+(c*d));
+	}
+}
