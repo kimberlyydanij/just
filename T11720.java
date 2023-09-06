8be3fa68c539ecb3589cc0b3944f10b50b6bd2b6
kimberlyydanij <kimberlydanij@gmail.com>
2023-09-06 23:41:39 +0900

    20230906 | Update

---
+++ T11720.java
@@ -0,0 +1,18 @@
+package coding_test;
+
+import java.util.Scanner;
+
+public class T11720 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int sum = 0;
+		int chk = sc.nextInt();
+		sc.nextInt();
+		String numbers = sc.next();
+		
+
+		sc.close();
+		System.out.println(sum);
+	}
+}
