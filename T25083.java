eb3176a269d066f54095ca4bb9f1813ea5f7c8e5
kimberlyydanij <kimberlydanij@gmail.com>
2023-09-12 22:00:19 +0900

    20230912 | UpdatE

---
+++ T25083.java
@@ -0,0 +1,13 @@
+package coding_test;
+
+public class T25083 {
+
+	public static void main(String[] args) {
+		System.out.println("         ,r'\"7");
+		System.out.println("r`-_   ,'  ,/");
+		System.out.println(" \\. \". L_r'");
+		System.out.println("   `~\\/");
+		System.out.println("      |");
+		System.out.println("      |");
+	}
+}
