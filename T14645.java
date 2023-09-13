babf7e2701062078cf4cfc72c811adc4ed75cd15
kimberlyydanij <kimberlydanij@gmail.com>
2023-09-13 23:32:08 +0900

    20230913 | Update

---
+++ T14645.java
@@ -0,0 +1,17 @@
+package coding_test;
+
+import java.util.Scanner;
+
+public class T14645 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		for (int i = 0;i < a; i++) {
+			String c = sc.nextLine();
+		}
+		sc.close();
+		System.out.println("비와이");
+	}
+}
