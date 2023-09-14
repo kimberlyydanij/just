002b0eacd078f92b7d6c5360ec5ae7dbc0d3ef86
kimberlyydanij <kimberlydanij@gmail.com>
2023-09-14 23:25:14 +0900

    20230914 | Update

---
+++ T10871.java
@@ -0,0 +1,18 @@
+package coding_test;
+
+import java.util.Scanner;
+
+public class T10871 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = 0;
+		for(int i = 1; i<=a;i++) {
+			c = sc.nextInt();
+			if(c<b)
+				System.out.printf("%d ",c);
+		}
+	}
+}
