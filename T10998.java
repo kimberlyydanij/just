c083ca53f21166288f845eeb1bd99202cb86b3dd
kimberlyydanij <kimberlydanij@gmail.com>
2023-09-17 23:54:08 +0900

    20230917 | Update

---
+++ T10998.java
@@ -0,0 +1,15 @@
+package coding_test;
+
+import java.util.Scanner;
+
+public class T10998 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		System.out.println(a*b);
+	}
+
+}
