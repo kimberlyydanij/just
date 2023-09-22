12bea90382e240638d350420d5a346136ab03f1b
kimberlyydanij <kimberlydanij@gmail.com>
2023-09-22 23:21:28 +0900

    20230922 | Update

---
+++ T10171.java
@@ -0,0 +1,11 @@
+package coding_test;
+
+public class T10171 {
+
+	public static void main(String[] args) {
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+	}
+}
